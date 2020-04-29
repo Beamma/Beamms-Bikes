bc6a95e0160a9e018a4b5a19bdd30131d9784348
Joel <17141@burnside.school.nz>
2020-04-30 00:04:04 +1200

    V1.1 Two Page Website

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/bikes')
+def bikes():
+    return render_template("bikes.html")
 
 if __name__ == "__main__":
     app.run(debug=True)

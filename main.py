730bdb81bdde7bb2ed4dca4f43c32c40343ed15b
Joel <17141@burnside.school.nz>
2020-04-30 23:45:16 +1200

    V3.0 Start Of Filter System

--- main.py
+++ main.py
@@ -11,10 +11,23 @@ def home():
 def bikes():
     conn = sqlite3.connect('Beamma-Bikes.db')
     c = conn.cursor()
-    c.execute("SELECT name, image FROM bikes")
+    c.execute("SELECT name, image FROM Bikes")
     bike = c.fetchall()
     conn.close()
     return render_template("bikes.html", bike = bike)
+
+@app.route('/bikes', methods=["GET","POST"])
+def filtered():
+    if request.method == 'POST':
+        post_id = request.form.get('Polygon')
+        if post_id is not None:
+            conn = sqlite3.connect('Beamma-Bikes.db')
+            c = conn.cursor()
+            c.execute("SELECT name, image FROM Bikes WHERE brand=1")
+            bike = c.fetchall()
+            conn.close()
+            print(post_id)
+            return render_template("bikes.html", bike = bike)
 
 if __name__ == "__main__":
     app.run(debug=True)

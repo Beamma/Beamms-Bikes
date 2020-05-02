bef2ae38b46d6ef3fff34d606a7ee59e84944cc6
Joel <17141@burnside.school.nz>
2020-05-02 18:30:02 +1200

    V3.1 Non Working Filter (Back Up)

--- main.py
+++ main.py
@@ -19,15 +19,68 @@ def bikes():
 @app.route('/bikes', methods=["GET","POST"])
 def filtered():
     if request.method == 'POST':
-        post_id = request.form.get('Polygon')
-        if post_id is not None:
-            conn = sqlite3.connect('Beamma-Bikes.db')
-            c = conn.cursor()
-            c.execute("SELECT name, image FROM Bikes WHERE brand=1")
-            bike = c.fetchall()
-            conn.close()
-            print(post_id)
-            return render_template("bikes.html", bike = bike)
+
+        # VARIABLES
+        t_count = 0
+        b_count = 0
+        f_count = 0
+        wheel = ""
+        s_filter = ""
+        where = ""
+        one_b = ""
+        two_b = ""
+        one_t = ""
+        two_t = ""
+        polygon = ""
+        trek = ""
+
+        # REQUESTS
+        polygon = request.form.get('polygon')
+        trek = request.form.get('trek')
+        type = request.form.get('type')
+
+        # CHECK FILTERS
+        if polygon is not None:
+            where = " WHERE"
+            polygon = "1"
+            b_count += 1
+
+        if trek is not None:
+            where = " WHERE"
+            trek ="1"
+            b_count += 1
+
+        if type is not None:
+            where = " WHERE"
+            type = "5"
+            t_count += 1
+
+
+        # ADDS "AND" IF NECCESARY
+        if b_count > 0:
+            one_b = " brand ="
+            f_count += 1
+
+        if b_count > 1:
+            two_b = " or "
+
+        if f_count > 0:
+            s_filter = " AND"
+
+        if f_count < 1:
+            s_filter = ""
+
+        if t_count > 0:
+            one_t = " type ="
+
+        if t_count > 1:
+            two_t = " or "
+        conn = sqlite3.connect('Beamma-Bikes.db')
+        c = conn.cursor()
+        c.execute("SELECT name, image FROM Bikes")
+        bike = c.fetchall()
+        conn.close()
+        return render_template("bikes.html", bike = bike)
 
 if __name__ == "__main__":
     app.run(debug=True)

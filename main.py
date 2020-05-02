9801a3043996b5482334578b0db4995c8892c21f
Joel <17141@burnside.school.nz>
2020-05-02 21:32:50 +1200

    V3.4 Added More Filter Options And More Bikes + A Ting Bit Of Css

--- main.py
+++ main.py
@@ -22,10 +22,14 @@ def bikes():
         # Requests
         r_polygon = request.form.get('polygon')
         r_trek = request.form.get('trek')
+        r_giant = request.form.get('giant')
+        r_liv = request.form.get('liv')
         r_enduro = request.form.get('enduro')
+        r_trail = request.form.get('trail')
+        r_ebike = request.form.get('ebike')
 
         # Add "WHERE" if a filter has been applied
-        if r_polygon or r_trek or r_enduro is not None:
+        if r_polygon or r_trek or r_giant or r_liv or r_trail or r_ebike or r_enduro is not None:
             query += "WHERE "
 
         # Check what filters have been applied
@@ -41,6 +45,30 @@ def bikes():
             query += "brand = 2 " # Add Filter
             fcount += 1
 
+        if r_giant is not None:
+            if fcount > 0:
+                query += "AND " # Check if "AND" is neccessary
+            query += "brand = 3 " # Add Filter
+            fcount += 1
+
+        if r_liv is not None:
+            if fcount > 0:
+                query += "AND " # Check if "AND" is neccessary
+            query += "brand = 4 " # Add Filter
+            fcount += 1
+
+        if r_trail is not None:
+            if fcount > 0:
+                query += "AND " # Check if "AND" is neccessary
+            query += "type = 3 " # Add Filter
+            fcount += 1
+
+        if r_ebike is not None:
+            if fcount > 0:
+                query += "AND " # Check if "AND" is neccessary
+            query += "type = 1 " # Add Filter
+            fcount += 1
+
         if r_enduro is not None:
             if fcount > 0:
                 query += "AND " # Check if "AND" is neccessary
@@ -49,7 +77,7 @@ def bikes():
 
         print(query) # Debug
 
-        # Connect to databse a preform query
+        # Connect to databse and preform query
         conn = sqlite3.connect('Beamma-Bikes.db')
         c = conn.cursor()
         c.execute(query)
@@ -60,7 +88,7 @@ def bikes():
     # Normal Page Loading
     else:
 
-        # Connect to databse a preform query
+        # Connect to databse and preform query
         conn = sqlite3.connect('Beamma-Bikes.db')
         c = conn.cursor()
         c.execute("SELECT name, image FROM Bikes")

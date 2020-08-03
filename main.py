942bd73fa31e539a3dda6c1b2aee9708e20f00b0
Joel <17141@burnside.school.nz>
2020-08-04 10:21:45 +1200

    V8.4 Adding Change Ability For Quantity

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def user():
         user_id = session.get('logstatus', None)
         conn = sqlite3.connect('Beamma-Bikes.db')
         c = conn.cursor()
-        c.execute("SELECT bikes.name, bikes.price, cart.quantity FROM cart INNER JOIN bikes ON cart.bike_id = bikes.id WHERE user_id=?", (user_id,))
+        c.execute("SELECT bikes.name, bikes.price, cart.quantity, cart.bike_id FROM cart INNER JOIN bikes ON cart.bike_id = bikes.id WHERE user_id=?", (user_id,))
         cart = c.fetchall()
         c.execute("SELECT name FROM users WHERE id=?",(user_id,))
         name = c.fetchall()
@@ -167,6 +167,12 @@ def user():
             if request.form.get("logout"):
                 session['logstatus'] = 'false'
                 return redirect(url_for('bikes'))
+            else:
+                for i in range(len(cart)):
+                    bike_id = cart[i]
+                    bike_quantity = request.form.get(bike_id[3])
+                    print(bike_id)
+                    print(bike_quantity)
         return render_template("user.html", logstatus = session.get('logstatus', None), cart = cart, name = name[0], price = price, quantity = quantity)
 
 if __name__ == "__main__":
